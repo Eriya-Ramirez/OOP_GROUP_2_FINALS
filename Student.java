a7a664f3d42124dfc60f507a97ce77ca90815b93
sciwhiz12 <arnoldnunag12@gmail.com>
2024-10-01 12:54:54 +0800

    Move Student class contents to proper place

--- Student.java
+++ Student.java
@@ -1,4 +1,31 @@
 
 public class Student {
+    private String Name, Gender, degreeProgram;
+    private int studentNo, enrollmentYear;
+    private float enrolledUnits;
 
+    public Student(String Name, String Gender, String degreeProgram, int studentNo, int enrollmentYear, float enrolledUnits){
+        this.Name = Name;
+        this.Gender = Gender;
+        this.degreeProgram = degreeProgram;
+        this.studentNo = studentNo;
+        this.enrollmentYear = enrollmentYear;
+        this.enrolledUnits = enrolledUnits;
+    }
+
+    public void enrollinCourse(){
+
+    }
+
+    public void dropCourse(){
+
+    }
+
+    public void viewCourses(){
+
+    }
+
+    public void viewGrades(){
+
+    }
 }
